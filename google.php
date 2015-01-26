47c2edfba9691838042515d16b37b9c9edaf85a3
Florian Marwitz <flo.marwitz@gmail.com>
2015-01-26 15:26:13 +0000

    added php/basic_head.php

--- google.php
+++ google.php
@@ -5,10 +5,7 @@
         
         <title>Suchmaschinen - Google</title>
         
-        <link rel="icon" href="img/monitor.jpg" type="image/jpg" />
-        
-        <link rel="stylesheet" type="text/css" href="css/style.css" />
-        <link rel="stylesheet" type="text/css" href="css/navigation.css" />
+        <?php include('php/basic_head.php') ?>
         
     </head>
     
---
+++ php/basic_head.php
@@ -0,0 +1,4 @@
+<link rel="icon" href="img/monitor.jpg" type="image/jpg" />
+        
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="css/navigation.css" />
