0f897f7b0c51c30a7712ea528767881f7d54da62
ToxycaL~ | Marius F. <mariusfunke99@web.de>
2015-02-03 17:37:19 +0100

    Added DropDown v.2 - fixed various aspects
    - DropDown Menü in nav_bar.php
    - DropDown Menü CSS in navigation.css
    - Impressum
    - Farbcodes gefixt

---
+++ impressum.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        
+        <title>Suchmaschinen</title>
+		
+		<!-- Basic Meta (kopiert aus index.php)-->
+		
+		<meta name="author" content="Florian Marwitz, Marius Funke">
+		<meta name="description" content="BESCHREIBUNG">
+		<meta name="keywords" content="KEYWORDS">
+		
+		<meta charset="UTF-8">
+        
+        <?php
+			include("php/basic_head.php");
+		?>
+		
+		<link rel="stylesheet" type="text/css" href="css/page/suchmaschinen.css" />
+        
+    </head>
+    
+    <body id="background">
+        
+        <div id="wrapper">
+            
+            <header>
+                
+                <!-- Navigation/Menü -->
+                <?php include('php/nav_bar.php') ?>
+                
+            </header>
+            
+            <main>
+                
+                <h2>Impressum</h2>
+                
+            </main>
+            
+        </div>
+        
+    </body>
+    
+</html>
