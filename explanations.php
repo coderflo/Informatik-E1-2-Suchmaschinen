9dc48e4bc5be4cb887ed00d7790606c491823a6b
Florian Marwitz <flo.marwitz@gmail.com>
2015-02-07 15:56:25 +0000

    changed content of explanations.php

--- explanations.php
+++ explanations.php
@@ -34,7 +34,13 @@
             
             <main>
                 
-                <h2>Inhaltsverzeichnis</h2>
+                <p class="admin_note">
+                    An dieser Seite wird gerade gearbeitet!
+                </p>
+                
+                <h1>Inhaltsverzeichnis</h1>
+                
+                <span class="admin_note">Inhaltsverzeichnis ist nicht mehr aktuell! Das Inhaltsverzeichnis wird aktualisiert, sobald der Inhalt vollst&auml;nidg ist!</span>
                 
                 <ol class="no_link">
                     
@@ -63,46 +69,56 @@
                     
                 </ol>
                 
-                <h1><a name="suchergebnisse">Suchergebnisse</a></h1>
-                
                 <p>
-                    Voll cooler Einleitungstext!
+                    
+                    Google durchsucht das Internet st&auml;ndig nach neuen Seiten und updated alte Sucheintr&auml;ge. Daf&uuml;r
+                    hat Google eine eigene Software, den Google Bot, oft aber auch Spider oder Crawler oder auch Webcrawler genannt, 
+                    die auf vielen Server weltweit l&auml;ft und die alle Links einer einzelnen
+                    Website besucht, und wieder deren Links und so weiter. Au&szlig;erdem werden alle Links und auch die Anzahl der Links
+                    zu einer bestimmten Website (zum Beispiel alle Links im Internet zu der Seite Amazon), wodurch der Rang dieser Seite
+                    (in unserem Beispiel Amazon) in den Suchergebnissssen ver&auml;ndert werden kann.
+                    
+                    Alle Ergebnisse werden in einer Datenbank gespeichert und diese einzelnen Datenbanken sind miteinander verkn&uuml;pft.
+                    
+                 </p>
+                    
+                <p>
+                    
+                    Es gibt mehrere M&ouml;glichkeiten, dem Google-Bot mittzuteilen, dass eine Seite nicht in die
+                    Suchergebnisse aufgenommen werden soll:
+                    <br>
+                    Die Erste ist der <code>no-follow</code> (<span class="admin_note">Bitte &uuml;berpr&uuml;fen!</span>) Tag.
+                    <br>
+                    F&uuml;r diesen Tag gibt es mehrere Werte, die das Verhalten des Bots einschr&auml;nken.
+                    <br>
+                    Die zweite M&ouml;glichkeit ist eine robots.txt Datei, die sich im Hauptverzeichnis der Website befindet.
+                    Die URL zu so einer Datei lautet dann zum Bespiel <a href="http://www.google.com/robots.txt">http://www.google.com/robots.txt</a>.
+                    <br>
+                    In dieser Datei kann dann eingestellt werden, welche Dateien oder Ordner nicht in die Suchergebnisse
+                    aufgenommen werden sollen.
+                    
                 </p>
                 
-                <h2><a name="bots">Bots</a></h2>
-                
                 <p>
-                    Richtig informativer Text &uuml;ber Suchmaschinenbots!
+                    
+                    Wenn du nun eine Suchanfrage bei Google eingibst, so urteilt Google nach mehreren Faktoren,
+                    welche Suchergebnisse zu angezeigst bekommst und an welcher Stelle der einzelne Sucheintrag steht.
+                    <br>
+                    Diese Faktoren sind zum Beispiel:
+                    <ul>
+                        <li>Standort</li>
+                        <li>Bestimmte Suchparameter</li>
+                        <li>Suchverlauf</li>
+                        <li>Ger&auml;t</li>
+                    </ul>
+                    Neben diesen gibt es allerdings noch zahlreiche andere Faktoren!
+                    
                 </p>
                 
-                <h2><a name="ranking">Ranking</a></h1>
-                
                 <p>
-                    Hier wird das Ranking der Suchergebnisse erl&auml;utert!
-                </p>
-                
-                <h2><a name="seo">Search Engine Optimization</a></h1>
-                
-                <p>
-                    Hier werdet ihr &uuml;ber SEO aufgekl&auml;rt!
-                </p>
-                
-                 <h1><a name="werbung">Werbung</a></h1>
-                
-                <p>
-                    Voll cooler Einleitungstext!
-                </p>
-                
-                <h2><a name="anzeigen">Werbeanzeigen</a></h2>
-                
-                <p>
-                    Richtig informativer Text &uuml;ber Anzeigen!
-                </p>
-                
-                <h2><a name="werbeprofil">Werbeprofil</a></h1>
-                
-                <p>
-                    Informationen &uuml;ber das Werbeprofil (was wei&szlig; Google &uuml;ber mich?)
+                    
+                    Platzhalter f&uuml;r einen kurzen Absatz &uuml;ber <a href="http://google.com/search?q=SEO">SEO</a>
+                    
                 </p>
                 
             </main>
